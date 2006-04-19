975e60a090ff793c197629909cf8b9941540ba89
arecasilla <arecasilla@da2a311f-2108-0410-ad33-ce5441d6e3f8>
2006-04-19 06:19:19 +0000

    Creating a tag of tags/astbill-0.9.17

---
+++ tags/astbill-0.9.17/wwwroot/ajax/README.txt
@@ -0,0 +1,14 @@
+Ajax and PHP implemented from AstBill-0.9.0.9
+
+This is Version: AstBill-0.9.0.10
+
+This files are using /home/astbill/astbill.conf  For database access/login.
+
+This is very simple AJAX technology
+
+Expect more to come later
+
+Remember the files in the ajax directory have to be one level down from your web root. In a sub directory called ajax.
+
+
+Are Casilla --
---
+++ tags/astbill-0.9.17/wwwroot/ajax/getchannel.php
@@ -0,0 +1,74 @@
+<?
+/*
+ * AstBill  -- Billing, Routing and Management software for Asterisk and MySQL using Drupal
+ *
+ * www.astbill.com
+ *
+ * Asterisk -- A telephony toolkit for Linux.
+ * Drupal   -- An Open source content management platform.
+ *
+ * 
+ * Copyright (C) 2005, AOFFICE NOMINEE SECRETARIES LIMITED, UNITED KINGDOM.
+ *
+ * Andreas Mikkelborg <adoroar [Guess What?] astartelecom.com>
+ * Are Casilla        <areast  [Guess What?] astartelecom.com>
+ *
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License
+ *
+ * 2006.03.17 Version 0.9.18
+ * 
+ */
+
+$filename = "/home/astbill/astbill.conf";
+$handle = @fopen($filename, "r");
+
+if ($handle) {
+   while (!feof($handle)) {
+       $buffer = fgets($handle);
+       $split = split("=", $buffer);
+       $var1 = trim($split['0']);
+       $var2 = trim($split['1']);
+       
+       if ($var1 == 'dbhost') {
+	       $dbhost = $var2;
+       }
+       
+       if ($var1 == 'dbname') {
+	       $dbname = $var2;
+       }
+       
+       if ($var1 == 'dbuser') {
+	       $dbuser = $var2;
+       }
+       
+       if ($var1 == 'dbpass') {
+	       $dbpass = $var2;
+       }
+
+   }
+   fclose($handle);
+}
+
+mysql_connect($dbhost,$dbuser,$dbpass);
+@mysql_select_db($dbname) or die("Unable to select database");
+
+$result = mysql_query("SELECT DISTINCT channel, accountcode, callednum, date_created, trunk FROM astcdr WHERE dialstatus is null");
+$pass = 0;
+
+while ($sql = mysql_fetch_object($result)) {
+echo $sql->accountcode.' '.$sql->callednum.' '.$sql->date_created.' '.$sql->channel.' '.$sql->trunk;
+print "\n";
+$pass = '1';
+}
+
+if ($pass == '0') {
+	echo 'no Channels in use';
+	print "\n";
+}
+
+mysql_free_result($result);
+mysql_close();
+
+?>
